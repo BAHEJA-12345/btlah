0c9253ea01608209acf8ff507d5b9838e81b7cb8
BAHEJA-12345 <84960446+BAHEJA-12345@users.noreply.github.com>
2025-11-05 09:10:44 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    df = pd.read_excel('btlah copy with pot size.xlsx')
+    return df.head().to_html()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
